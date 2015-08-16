74e598c7be8cb8126bd7e71bc43dd748742deecc
Jon Rosebaugh <jon@inklesspen.com>
2015-08-16 19:21:11 -0400

    Send proper id type in params.

--- mimir/frontend/components/WriteupDetail.jsx
+++ mimir/frontend/components/WriteupDetail.jsx
@@ -6,7 +6,11 @@ import {WriteupDetailStore} from '../stores/WriteupStore';
 export default class WriteupDetail extends React.Component {
     static willTransitionTo(transition, params, query, callback) {
         // maybe would be better to use transition.abort() and transition.retry()
-        var p = WriteupDetailStore.fetchWriteup(params.id);
+        const id = parseInt(params.id, 10);
+        if (isNaN(id)) {
+            return callback(id);
+        }
+        var p = WriteupDetailStore.fetchWriteup(id);
         p.then(function() {
             callback();  // success
         }).catch(function(error) {
