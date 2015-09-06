841f402e00546097032885ffd009074cb622d56f
Jon Rosebaugh <jon@inklesspen.com>
2015-09-06 14:39:37 -0400

    Read-only Post detail and versions list
    Time to focus on some backend stuff before I finish this page.

--- mimir/frontend/components/post_detail/PostOverview.jsx
+++ mimir/frontend/components/post_detail/PostOverview.jsx
@@ -1,10 +1,92 @@
 import React from 'react';
+import Immutable from 'immutable';
+import {FormRowInput, FormRowCheckbox, FormRowSelect} from '../FormRowTools.jsx';
+
+function mapWithDefaults(post) {
+    let map = Immutable.Map({
+        'author': '',
+        'index': 0,
+        'ordinal': '',
+        'title': ''
+    });
+    map = map.merge(post);
+    return map;
+}
+
 
 export class PostOverview extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = this.resetState(props);
+    }
+
+    resetState(props) {
+        const creating = !props.post;
+        return {
+            creating,
+            // editable by default if creating
+            editable: creating,
+            post: mapWithDefaults(props.post)
+        };
+    }
+
+    edit() {
+        this.setState({editable: true});
+    }
+
+    save() {
+        let value = this.state.post.toJS();
+        console.log("savePost", value);
+    }
+
+    cancel() {
+        this.setState(
+            {
+                editable: false,
+                post: mapWithDefaults(this.props.post)
+            }
+        );
+    }
+
+    changeHandler(field) {
+        return (value) => {
+            this.setState({post: this.state.post.set(field, value)});
+        };
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState(this.resetState(nextProps));
+    }
+
     render() {
-        console.log(this.props);
+        let btns;
+        if (this.state.editable) {
+            btns = (
+                <div className="btn-group" role="group">
+                    <button type="button" className="btn btn-success" onClick={this.save.bind(this)}>Save</button>
+                    <button type="button" className="btn btn-danger" onClick={this.cancel.bind(this)}>Cancel</button>
+                </div>
+            );
+        } else {
+            btns = (<button type="button" onClick={this.edit.bind(this)} className="btn btn-default" >Edit</button>);
+        }
         return (
-            <div className="fnord">Fnord</div>
+            <div className="row">
+                <div className="col-md-12">
+                    <form className="form-horizontal">
+                        <FormRowInput editable={this.state.editable} label="Author" value={this.state.post.get('author')} data-id='author' onChange={this.changeHandler('author')} />
+                        <FormRowInput editable={this.state.editable} label="Ordinal" value={this.state.post.get('ordinal')} data-id='ordinal' onChange={this.changeHandler('ordinal')} />
+                        <FormRowInput editable={this.state.editable} label="Title" value={this.state.post.get('title')} data-id='title' onChange={this.changeHandler('title')} />
+
+                        <div className="form-group">
+                            <div className="col-sm-offset-2 col-sm-10">
+                                {btns}
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
         );
     }
 }
--- mimir/frontend/components/post_detail/VersionList.jsx
+++ mimir/frontend/components/post_detail/VersionList.jsx
@@ -1,10 +1,67 @@
 import React from 'react';
+import classNames from 'classnames';
+
+class Version extends React.Component {
+    render() {
+        var classes = classNames(
+            "fa", "fa-lg",
+            (this.props.version.active ? "fa-check-square-o" : "fa-square-o"),
+        );
+        return (
+            <tr>
+                <td><a role="button" href="#" onClick={this.props.onSelect}>{this.props.version.version}</a></td>
+                <td>N/A yet</td>
+                <td><span className={classes}></span></td>
+                <td>TBD</td>
+            </tr>
+        );
+    }
+}
+
 
 export class VersionList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedIndex: null
+        };
+    }
+    clickHandler(index) {
+        return () => {
+            console.log(index);
+            this.setState({selectedIndex: index});
+        };
+    }
+
+    getHtmlView() {
+        if (this.state.selectedIndex !== null) {
+            return {
+                __html: this.props.post.versions[this.state.selectedIndex].html
+            };
+        }
+    }
+
     render() {
-        console.log(this.props);
         return (
-            <div className="fnord">Fnord</div>
+            <div>
+                <table className="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>Version</th>
+                            <th>Summary</th>
+                            <th>Active</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {this.props.post.versions.map((version, index) => {
+                        return (<Version key={version.version} version={version} onSelect={this.clickHandler(index)}/>);
+                     })}
+                    </tbody>
+                </table>
+                <hr />
+                <div dangerouslySetInnerHTML={this.getHtmlView()}></div>
+            </div>
         );
     }
 }
